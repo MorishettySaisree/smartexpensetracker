08bb6e779dd1f0a550c3d4a77de1f1145782f405
MorishettySaisree <saisreemorishetty@gmail.com>
2026-04-02 19:05:00 +0530

    Add home controller

---
+++ backend/src/main/java/com/smartexpense/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.smartexpense.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Smart Expense Tracker backend is running successfully";
+    }
+}
